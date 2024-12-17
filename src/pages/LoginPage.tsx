3982a60701e930feab4992aff0ea41028457f933
Jezebel1990 <jezebelguedes@gmail.com>
2024-12-17 04:36:33 -0300

    fix: update text size

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -17,7 +17,6 @@ export default function LoginPage() {
   }, []);
 
   useEffect(() => {
-    console.log("Verificando estado 'isLoggedin':", isLoggedin);
     if (isLoggedin) {
       navigate("/"); 
     }
@@ -40,21 +39,22 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-abstract">
       <div className="text-center mb-6">
-        <h1 className="text-3xl font-light text-gray-200">
+        <h1 className="text-xl sm:text-3xl font-light text-gray-200">
           Seja bem-vindo ao
           <span className="font-semibold text-white"> GEN A</span>
           <span className="text-[#2afbc2] font-semibold">i</span>
         </h1>
-        <div className="w-320 h-80 mt-8 bg-white/20 shadow-lg rounded-lg flex flex-col items-center justify-center mb-8">
+        <div className="w-260 sm:w-320 h-80 mt-8 bg-white/20 shadow-lg rounded-lg flex flex-col items-center justify-center mb-8">
           <img
             src="https://r2.erweima.ai/imgcompressed/compressed_300167030ea6a6ab25faa7a64e128960.webp"
             alt="Visitante"
             className="w-36 h-36 rounded-full border-4 border-white my-4"
           />
-          <h2 className="text-3xl mt-4 text-white">
+          <h2 className="text-xl sm:text-3xl mt-4 text-white">
             Olá, <span className="text-[#3ff1b1] font-semibold">visitante</span>!
           </h2>
         </div>
+        <div className="flex justify-center">
         <GoogleLogin
           onSuccess={handleSuccess}
           onError={handleError}
@@ -63,6 +63,7 @@ export default function LoginPage() {
           shape="rectangular"
           width="420"
         />
+      </div>
       </div>
       </div>
   );
